12c0d7f572207c7ba8f711f36ffc65792d0ecd82
tygavin <tianyao@zju.edu.cn>
2019-08-23 22:22:25 +0800

    [add] 添加inject 与background的交互信息通路

---
+++ public/js/content-script.js
@@ -0,0 +1,56 @@
+(function() {
+	console.log('这是 content-script！');
+})();
+
+// 向页面注入JS
+(function injectCustomJs(jsPath)
+{
+    jsPath = jsPath || 'js/inject.js';
+	var temp = document.createElement('script');
+	console.log(temp);
+	console.log("正在注入inject.js 呀呀呀")
+    temp.setAttribute('type', 'text/javascript');
+    // 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
+	temp.src = chrome.extension.getURL(jsPath);
+	console.log(temp.src);
+	console.log(document)
+    temp.onload = function()
+    {
+        // 放在页面不好看，执行完后移除掉
+        this.parentNode.removeChild(this);
+	};
+	console.log(document.head)
+    document.body.appendChild(temp);
+}())
+
+// 接收来自后台的消息
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{
+	console.log('收到来自 ' + (sender.tab ? "content-script(" + sender.tab.url + ")" : "popup或者background") + ' 的消息：', request);
+});
+
+// 主动发送消息给后台
+// 要演示此功能，请打开控制台主动执行sendMessageToBackground()
+function sendMessageToBackground(message) {
+	chrome.runtime.sendMessage({greeting: message || '你好，我是content-script呀，我主动发消息给后台！'}, function(response) {
+		console.log("我在content-script,主动发消息给background，回复是：",response);
+	});
+};
+
+// 接收来自inject的消息
+window.addEventListener("message", function(e)
+{
+	console.log("我在content-script,我收到了inject传来的消息：",e);
+	
+}, false);
+
+chrome.runtime.onConnect.addListener(function(port) {
+	console.log(port);
+	if(port.name == 'test-connect') {
+		port.onMessage.addListener(function(msg) {
+			console.log('收到长连接消息：', msg);
+			tip('收到长连接消息：' + JSON.stringify(msg));
+			if(msg.question == '你是谁啊？') port.postMessage({answer: '我是你爸！'});
+		});
+	}
+});
---
+++ public/js/inject.js
@@ -0,0 +1,165 @@
+/*自定义promise*/
+function MyPromise(fn) {
+    this.value;
+    this.status = 'pending';
+    this.resolveFunc = function () { };
+    this.rejectFunc = function () { };
+    fn(this.resolve.bind(this), this.reject.bind(this));
+}
+console.log("inject into!");
+
+/*
+***生成随机回调函数名称***
+*/
+var getCallbackName = function () {
+    var random = parseInt(Math.random() * 100000)
+    return 'pra_callback_' + new Date().getTime() + random
+}
+
+MyPromise.prototype = {
+    resolve: function (val) {
+        var self = this;
+        if (this.status == 'pending') {
+        this.status = 'resolved';
+        this.value = val;
+        setTimeout(function () {
+            self.resolveFunc(self.value);
+        }, 0);
+        }
+    },
+    reject: function (val) {
+        var self = this;
+        if (this.status == 'pending') {
+        this.status = 'rejected';
+        this.value = val;
+        setTimeout(function () {
+            self.rejectFunc(self.value);
+        }, 0);
+        }
+    },
+    then: function (resolveFunc, rejectFunc) {
+        var self = this;
+        return new MyPromise(function (resolve_next, reject_next) {
+        function resolveFuncWrap() {
+            var result = resolveFunc(self.value);
+            if (result && typeof result.then === 'function') {
+            result.then(resolve_next, reject_next);
+            } else {
+            resolve_next(result);
+            }
+        }
+        function rejectFuncWrap() {
+            if (typeof rejectFunc !== 'function') {
+            rejectFunc = function () { return self.value };
+            }
+            var result = rejectFunc(self.value);
+            if (result && typeof result.then === 'function') {
+            result.then(resolve_next, reject_next);
+            } else {
+            reject_next(result);
+            }
+        }
+        self.resolveFunc = resolveFuncWrap;
+        self.rejectFunc = rejectFuncWrap;
+        })
+    },
+    catch: function (resolveFunc) {
+        var self = this;
+        return new MyPromise(function (resolve_next, reject_next) {
+        function resolveFuncWrap() {
+            if (self.status !== 'resolved') {
+            var result = resolveFunc(self.value);
+            if (result && typeof result.then === 'function') {
+                result.then(resolve_next, reject_next);
+            } else {
+                resolve_next(result);
+            }
+            }
+        }
+
+        function rejectFuncWrap() {
+            var result = resolveFunc(self.value);
+            if (result && typeof result.then === 'function') {
+            result.then(resolve_next, reject_next);
+            } else {
+            resolve_next(result);
+            }
+        }
+        self.resolveFunc = resolveFuncWrap;
+        self.rejectFunc = rejectFuncWrap;
+        })
+    }
+}
+
+/*
+***********转发请求到原生************
+***@methodName 方法名，字符串，不可为空
+***@contractName 合约名，字符串，可为空
+***@actionName  行为名，字符串，可为空
+***@params 附加参数，json字符串，可为空
+***@cbName 回调函数名称，字符串，不可为空
+*/
+let sendPraRequest = function (methodName, contractName, actionName, params, cbName) {
+    window.postMessage({ type: methodName, params: params, msg: cbName }, '*');
+}
+
+function Pra() {
+    this.isExtension = true;
+    this.version = '1.0.0';
+  
+    window.addEventListener('message', function (event) {
+        this.console.log("我在inject.js，收到content-script的消息");
+    }, false);
+  }
+
+Pra.prototype.baba = "Gaotianyao"
+
+/*
+***获得weID
+***@return 返回weID
+*/
+Pra.prototype.getWeID = function () {
+    return new Promise(function (resolve, reject) {
+        var praCallbackFun = getCallbackName();
+        window[praCallbackFun] = function (res) {
+            try {
+                resolve(res);
+                this.console.log("getWeID:",res)
+            } catch (e) {
+                reject(e);
+            }
+        }
+        sendPraRequest('getWeID', '', '', '', praCallbackFun);
+    });
+}
+
+/*
+***授权凭证
+***@return 返回weID
+*/
+Pra.prototype.getCredential = function (organization) {
+    return new Promise(function (resolve, reject) {
+        var praCallbackFun = getCallbackName();
+        window[praCallbackFun] = function (res) {
+            try {
+                resolve(res);
+                /*
+                    {
+                        status:"",   0/1
+                        data:{
+
+                        }     
+                    }
+                */ 
+                this.console.log("getCredential:",res)
+            } catch (e) {
+                reject(e);
+            }
+        }
+        sendPraRequest('getCredential', '', '', {name: organization}, praCallbackFun);
+    });
+}
+
+if (typeof window !== 'undefined') {
+    window.weID = new Pra();
+  }
